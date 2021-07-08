fe053896fe55f851ca3a95421216c1c967a2a17d
kazuma <69296965+S1270199KAzuma@users.noreply.github.com>
2021-07-08 17:30:02 +0900

    Add files via upload

---
+++ Dice.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int Dice1,Dice2;
+
+    srand((unsigned int)time(NULL));
+    
+    Dice1 = rand() % 6 + 1;
+    Dice2 = rand() % 6 + 1;
+
+    printf("Rolling the Dice...\n");
+    printf("Die 1: %d\n",Dice1);
+    printf("Die 2: %d\n",Dice2);
+    printf("Total value: %d\n",Dice1 + Dice2);
+}
