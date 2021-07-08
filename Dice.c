fc9bb0319d73ad3dfaeae71e9f3732b85636dc07
kazuma <69296965+S1270199KAzuma@users.noreply.github.com>
2021-07-08 17:55:50 +0900

    Update Dice.c
    Won/lost message added

--- Dice.c
+++ Dice.c
@@ -13,5 +13,12 @@ int main(){
     printf("Die 2: %d\n",Dice2);
     printf("Total value: %d\n",Dice1 + Dice2);
     
+    if((Dice1 + Dice2) > 7){
+        printf("You won\n");
+    }
+    else{
+        printf("You lose\n");
+    }
+    
     return 0;
 }
